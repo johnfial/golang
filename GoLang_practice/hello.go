9bd842fcb2d01ae02004bc000d1930451b160d10
johnfial <12399190+johnfial@users.noreply.github.com>
2021-07-30 11:46:30 -0700

    5min practice

--- GoLang_practice/hello.go
+++ GoLang_practice/hello.go
@@ -3,19 +3,28 @@
 // https://ispycode.com/GO/Path-and-Filepath/Get-file-name-extension-used-by-path
 
 // tutorials reccomended:
-	// https://learnxinyminutes.com/docs/go/
-	// and maybe https://gobyexample.com/
-
+// https://learnxinyminutes.com/docs/go/
+// and maybe https://gobyexample.com/
 
 // this doc: https://golang.org/doc/tutorial/getting-started
 
+// run with:
+// "go run hello.go"
+// "go run ." // saw this elsewhere, not sure if runs all files, looks for some kind of Go-"project", or what...
 package main
-import(
-    "fmt"
+
+import (
+	"fmt"
 )
-func main() {
-    fmt.Println("hello world")
-	fmt.Print('this line') 		// .\hello.go:18:12: more than one character in rune literal
+
+var printme = "'variable'" // language looks like JS here, no?
+
+func main() { // main() runs like python's if __name__ == __main__
 	fmt.Print("that line")
-	fmt.Printf('number bytes') // .\hello.go:20:13: more than one character in rune literal
+	fmt.Println("hello world") //prints on SAME line as above, new line AFTER this ends
+	fmt.Println(printme)
+
+	// single quotes =(
+	// fmt.Print('this line') 		// .\hello.go:18:12: more than one character in rune literal
+	// fmt.Printf('number bytes') // .\hello.go:20:13: more than one character in rune literal
 }
